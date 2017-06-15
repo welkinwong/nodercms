033b6d04319417bfd95e65fe335effda920d3126
welkinwong <welkin.wong@me.com>
2017-06-16 01:29:51 +0800

    Increase compare helper

---
+++ core/helpers/compare.helper.js
@@ -0,0 +1,29 @@
+module.exports = function (left, operator, right, options) {
+  if (arguments.length < 3) {
+    throw new Error('Handlerbars Helper "compare" needs 2 parameters');
+  }
+
+  var operators = {
+    '==': function(l, r) { return l == r },
+    '===': function(l, r) { return l === r },
+    '!=': function(l, r) { return l != r },
+    '!==': function(l, r) { return l !== r },
+    '<': function(l, r) { return l < r },
+    '>': function(l, r) { return l > r },
+    '<=': function(l, r) { return l <= r },
+    '>=': function(l, r) { return l >= r },
+    'typeof': function(l, r) { return typeof l == r }
+  };
+
+  if (!operators[operator]) {
+    throw new Error('Handlerbars Helper "compare" doesn\'t know the operator ' + operator);
+  }
+
+  var result = operators[operator](left, right);
+
+  if (result) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+};
